046155b1c3e262904db01360fc5c6e64309a1223
missMarPl <young.miss.marpl@gmail.com>
2015-05-27 20:47:41 +0300

    Server Settings
    Добавлено окно и функционал для настройки почтового сервера

--- WindowsFormsApplication1/Form_Main.cs
+++ WindowsFormsApplication1/Form_Main.cs
@@ -6,19 +6,97 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Data.OleDb;
 
 namespace WindowsFormsApplication1
 {
     public partial class Form_Main : Form
     {
+
+        string  currentDate;
+        public void format_date()
+        {
+            currentDate = DateTime.Now.Date.ToString();
+            currentDate = currentDate.Substring(0, 5);
+        }
+
         public Form_Main()
         {
             InitializeComponent();
            // checkedListBox1.SetItemChecked(1, true);
            // checkedListBox1.GetItemChecked(1);
+            format_date();
+		   main_comboBox.Items.Clear();
+
+		    OleDbConnection conn = new OleDbConnection();
+            //устанавливаем строку подключения для данного объекта-подключения
+            conn.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0; Data Source=H:\db_congrat.accdb";
+            //создаем команду
+            //передаем sql-запрос команде
+            OleDbCommand cmd = new OleDbCommand("SELECT HolName FROM Holiday WHERE HolDate='"+currentDate+"'",conn);
+            //открываем подключение
+            conn.Open();
+            //выполняем команду и создаем объект чтения данных
+            OleDbDataReader dr = cmd.ExecuteReader();
+            //метод Read() считывает данные строка за строкой
+            while (dr.Read())
+            {
+                //в данной строке мы выбираем данные хранящиеся в столбце мое_поле
+                string str = dr.GetString(dr.GetOrdinal("HolName"));
+                //записываем данные в коллекцию хранящую пункты в комбобокс
+               main_comboBox.Items.Add(str);
+            }
+            //закрываем объект чтения данных и объект подключения
+            dr.Close();
+            conn.Close();
+            main_comboBox.Items.Add("День рождения");
         }
+		
 
+	private void main_comboBox_SelectionChanged(object sender, EventArgs e)
+    {
+        main_checkedListBox.Items.Clear();
+        string comboItem = main_comboBox.SelectedItem.ToString();
+        
 
+		   OleDbConnection conn = new OleDbConnection();
+            //устанавливаем строку подключения для данного объекта-подключения
+            conn.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0; Data Source=H:\db_congrat.accdb";
+            //создаем команду
+            OleDbCommand cmd = conn.CreateCommand();
+			//передаем sql-запрос команде
+
+					switch (comboItem)
+{
+    case "День рождения":
+        cmd.CommandText="SELECT FrName FROM Friend WHERE Birthday LIKE '"+currentDate+"%'";
+        break;
+    case "8 марта":
+        cmd.CommandText = "SELECT FrName FROM Friend WHERE Sex='ж'";
+        break;
+    default:
+        cmd.CommandText = "SELECT FrName FROM Friend";
+        break;
+}
+            //открываем подключение
+            conn.Open();
+            //выполняем команду и создаем объект чтения данных
+            OleDbDataReader dr = cmd.ExecuteReader();
+            //метод Read() считывает данные строка за строкой
+            while (dr.Read())
+            {
+                //в данной строке мы выбираем данные хранящиеся в столбце мое_поле
+                string str = dr.GetString(dr.GetOrdinal("FrName"));
+                //записываем данные в коллекцию хранящую пункты в листбокс
+               main_checkedListBox.Items.Add(str);
+            }
+            //закрываем объект чтения данных и объект подключения
+            dr.Close();
+            conn.Close();        
+    }
+
+    
+		
         private void dropData_button_Click(object sender, EventArgs e) //сбросить
         {
             main_checkAll_button.Visible = true;
@@ -64,6 +142,13 @@ namespace WindowsFormsApplication1
             form.Show();
         }
 
+        private void настройкиToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MailSettings form = new MailSettings();
+            form.Location = new Point(200, 300);
+            form.Show();
+        }
+
         private void Form_Main_FormClosing(object sender, FormClosingEventArgs e)
         {
             //Form_PopUp frm = new Form_PopUp();
@@ -77,7 +162,15 @@ namespace WindowsFormsApplication1
             Application.Exit();
         }
 
+        private void main_send_button_Click(object sender, EventArgs e)
+        {
+            int y = main_checkedListBox.CheckedItems.Count;
+            string[] selected_names = new string[y];
+            main_checkedListBox.CheckedItems.CopyTo(selected_names, 0);
 
+            Generator gen = new Generator(selected_names, main_comboBox.SelectedItem.ToString());
+            
+        }
 
     
 
--- WindowsFormsApplication1/Form_PopUp.cs
+++ WindowsFormsApplication1/Form_PopUp.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Data.OleDb;
 
 namespace WindowsFormsApplication1
 {
@@ -15,6 +16,43 @@ namespace WindowsFormsApplication1
         {
             InitializeComponent();
             date_label.Text = DateTime.Now.ToString("dd MMMM yyyy");;
+
+            popUp_borntoday_list.Items.Clear();
+
+            OleDbConnection conn = new OleDbConnection();
+            //устанавливаем строку подключения для данного объекта-подключения
+            conn.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0; Data Source=H:\db_congrat.accdb";
+            //создаем команду
+            //передаем sql-запрос команде
+            OleDbCommand cmd = new OleDbCommand("SELECT FrName FROM Friend WHERE Birthday LIKE '" + DateTime.Now.Date.ToString().Substring(0, 5) + "%'", conn);
+            OleDbCommand cmd2 = new OleDbCommand("SELECT HolName FROM Holiday WHERE HolDate='" + DateTime.Now.Date.ToString().Substring(0, 5) + "'", conn);
+            //открываем подключение
+            conn.Open();
+            //выполняем команду и создаем объект чтения данных
+            OleDbDataReader dr = cmd.ExecuteReader();
+            //метод Read() считывает данные строка за строкой
+            while (dr.Read())
+            {
+                //в данной строке мы выбираем данные хранящиеся в столбце мое_поле
+                string str = dr.GetString(dr.GetOrdinal("FrName"));
+                //записываем данные в коллекцию хранящую пункты в комбобокс
+                popUp_borntoday_list.Items.Add(str);
+            }
+            //закрываем объект чтения данных и объект подключения
+            dr.Close();
+
+            OleDbDataReader dr2 = cmd2.ExecuteReader();
+            //метод Read() считывает данные строка за строкой
+            while (dr2.Read())
+            {
+                //в данной строке мы выбираем данные хранящиеся в столбце мое_поле
+                string str = dr2.GetString(dr2.GetOrdinal("HolName"));
+                //записываем данные в label списка праздников
+                popUp_todaysholiday_label.Text = popUp_todaysholiday_label.Text + " " + str;
+            }
+            //закрываем объект чтения данных и объект подключения
+            dr2.Close();
+            conn.Close();
         }
 
         private void Form_PopUp_Load(object sender, EventArgs e)
